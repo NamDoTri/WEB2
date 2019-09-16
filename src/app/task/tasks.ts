74ee40f18034a47da137e0800eec8d183ef23268
Nina Delcroix <n.delcroix@student.fontys.nl>
2019-09-16 09:17:09 +0200

    fixed error on merge

--- src/app/task/tasks.ts
+++
@@ -1,7 +0,0 @@
-import {Task} from './task';
-
-export let tasks = <Task[]>[
-    {id: 1, des: "First", completed: false},
-    {id: 2, des: "Second", completed: false},
-    {id: 3, des: "Third", completed: false}
-  ]
