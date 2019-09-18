d6abfa56f8fc9df834af3d1cdb50efce3d124910
Do Tri Nam <t8dotr00@students.oamk.fi>
2019-09-18 09:28:11 +0200

    resolved conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,18 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import { EmployeeComponent } from './employee/employee.component';
-<<<<<<< src/app/app.module.ts
-import { DepartmentComponent } from './department/department.component';
-=======
+import {DepartmentComponent} from './department/department.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
->>>>>>> src/app/app.module.ts
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
     EmployeeComponent,
-    DepartmentComponent
+    DepartmentComponent,
     LayoutComponent,
     HomeComponent
   ],
--- src/app/employee/employees.ts
+++
@@ -1,7 +0,0 @@
-import {Employee} from './employee';
-
-export let employees = <Employee[]>[
-    {id: 1, name: "John", dpt: "Sales"},
-    {id: 2, name: "Laura", dpt: "Human Resource"},
-    {id: 3, name: "Kim", dpt: "Technology"},
-]
