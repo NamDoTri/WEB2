113c0a44c65d6541dfa88e8823d53f16b011e5c5
Do Tri Nam <t8dotr00@students.oamk.fi>
2019-09-18 09:50:50 +0200

    added employeeService

---
+++ src/app/employee.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import Employee from './employee/employee';
+import {employeeArr} from './employee/employeeArr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  employeeArr: Employee[] = employeeArr;
+
+  constructor() { }
+  
+  getEmployees(){
+    return <Employee[]> this.employeeArr;
+  }
+}
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { employeeArr } from './employeeArr';
 import Employee from './employee';
-
+import {EmployeeService} from '../employee.service';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
-  employee: Employee[] = employeeArr;
+  employees: Employee[] = this.employeeService.getEmployees();
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
   }
