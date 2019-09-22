361428eb9b1cc4316ef8f086660301575878abdb
Nina Delcroix <n.delcroix@student.fontys.nl>
2019-09-22 08:30:19 +0200

    added crud on task component

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -9,10 +9,33 @@ import { taskArr } from './taskArr';
 })
 export class TaskComponent implements OnInit {
   task: Task[] = taskArr;
+  displayed: number = -1;
+  modified: boolean = false;
 
-  constructor() { }
+  ngOnInit() {  }
 
-  ngOnInit() {
+  updateDisplayed(idx) {
+    if (idx !== this.displayed) {
+      this.modified = false;
+      this.displayed = idx;
+    } else {
+      this.displayed = -1;
+      this.modified = false;
+    }
   }
 
+  newTask(taske) {
+    const taskToPush: Task = { id: taske.id, name: taske.name, description: taske.description}
+    this.task.push(taskToPush);
+  }
+
+  updateTask(taske) {
+    this.task[this.displayed] = taske;
+    this.modified = false;
+  }
+
+  deleteTask(taske) {
+    this.task.splice(taske, 1);
+    this.displayed = -1;
+  }
 }
