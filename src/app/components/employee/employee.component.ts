a9d5d14ac5291487d1044860ad8ba1cf73d5ac46
Do Tri Nam <t8dotr00@students.oamk.fi>
2019-10-07 23:53:24 +0200

    Fixed Delete feature of Employee

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -11,22 +11,17 @@ import { Employee } from 'src/app/models/employee.model';
 export class EmployeeComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
   employee: Employee[] = [];
-  modifying: boolean = false;
-  displayed: number = -1;
 
   ngOnInit() {
     this.reload();
   }
 
   reload() {
-    this.modifying = false;
-    this.displayed = -1;
     this.employeeService.getEmployee()
       .subscribe((res: Employee[]) => this.employee = res);
   }
 
   removeEmployee(toRemove) {
-    this.displayed = -1;
     this.employeeService.removeEmployee(toRemove)
       .subscribe(() => this.reload())
   }
