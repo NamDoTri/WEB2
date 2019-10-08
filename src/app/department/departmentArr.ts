1ac02d42d98786d737a60b2e0d10b99bdd050e29
Nina Delcroix <n.delcroix@student.fontys.nl>
2019-10-08 20:05:05 +0200

    rm

--- src/app/department/departmentArr.ts
+++
@@ -1,7 +0,0 @@
-import {Department} from './department';
-
-export const departmentArr = <Department[]>[
-    {id: 1, name: "Sales", employees: 4},
-    {id: 2, name: "Technology", employees: 4},
-    {id: 3, name: "Human Resource", employees: 4}
-]
